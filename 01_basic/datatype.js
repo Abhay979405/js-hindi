f97202d1f294d5ee3f4a6b310b24cf9b09d6a5ee
Abhay979405 <abhaydwivedi5070@gmail.com>
2026-06-13 21:02:09 +0530

    datatypes understanding

---
+++ 01_basic/datatype.js
@@ -0,0 +1,20 @@
+"use strict";//treat all js code as new version
+
+//alert("hello"); // we are using node.js node browser
+
+//console.log(3
+  //  +3
+//) // code readiablity should be high
+
+let name="abhay";
+let age=10;
+let isloggedIn=false;
+
+//number biginit
+//string
+//boolean
+//null= standalone value h 
+//undefined k mtlb h memory defined ho gyi h but usme koi value nhi daali gyi
+//symbol= mostly react m use hota h comopnent k bich m uniqueness btane k liye 
+console.log(typeof undefined);//y undefined hi dega 
+console.log(typeof null)//y type object dega
--- 01_basic/variable.js
+++ 01_basic/variable.js
@@ -1,1 +1,8 @@
-console.log("abhay");
+//console.log("abhay");
+const account_id=12344;
+let account_email="abhay@gmail.com";
+var account_password="12334";
+account_city="jaipur";
+//account_id=2;
+//account_city="bengaluru";
+console.table([account_id, account_email, account_password,account_city]);
