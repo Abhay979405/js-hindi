51d97889bb3cc2bb7b98cb198c9d980aa3b1f48e
Abhay979405 <abhaydwivedi5070@gmail.com>
2026-06-13 21:49:51 +0530

    comparision understanding

---
+++ 01_basic/comparision.js
@@ -0,0 +1,14 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+//console.log("2">1);//y true return krta h
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+//=== y strict check krta h datatype bhi check krta h 
+console.log("2"===2);//yha false aayga datatype alg h 
