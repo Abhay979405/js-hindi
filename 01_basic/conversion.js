0f22795a7f15360c16005b0184479fcb80bcbe6f
Abhay979405 <abhaydwivedi5070@gmail.com>
2026-06-13 21:37:28 +0530

    operations

--- 01_basic/conversion.js
+++ 01_basic/conversion.js
@@ -1,17 +1,54 @@
-let score="33";
-console.log(typeof score);
+// let score="33";
+// console.log(typeof score);
 
-let valueinnumber=Number(score);
-console.log(typeof(valueinnumber));
+// let valueinnumber=Number(score);
+// console.log(typeof(valueinnumber));
 
 //"33"=>33;
 //true=>1,false=>0;
 //"33abc"=>NaN;
 
-let isloggedin=1;
-let con=Boolean(isloggedin);
-console.log(typeof con);
+// let isloggedin=1;
+// let con=Boolean(isloggedin);
+// console.log(typeof con);
 
-let somenumber=33;
-let str=String(somenumber);
-console.log(typeof str);
+// let somenumber=33;
+// let str=String(somenumber);
+// console.log(typeof str);
+
+//******************operations*******/
+
+let value=3;
+let negativevalue=-value;
+//console.log(negativevalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1="hello";
+let str2="abhay";
+let str3=str1+" "+str2;
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);//agr string phele h to usko leke hi sb kuch treat kiya jayga
+console.log(1+2+"2");//agr phela no number h to number ki trh treat krenge starting ko
+
+// let gamecounter=100;
+// gamecounter++;
+// console.log(gamecounter);
+
+//postfix
+let b = 5;
+console.log(b++); // 5 (pehle return, fir increment)
+console.log(b);   // 6
+//prefix
+
+let a = 5;
+console.log(++a); // 6 (pehle increment, fir return)
+console.log(a);   // 6
