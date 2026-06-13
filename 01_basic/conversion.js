6d8624fa58406dd196674e2c3082d0abec37feff
Abhay979405 <abhaydwivedi5070@gmail.com>
2026-06-13 21:15:38 +0530

    conversion understanding

---
+++ 01_basic/conversion.js
@@ -0,0 +1,17 @@
+let score="33";
+console.log(typeof score);
+
+let valueinnumber=Number(score);
+console.log(typeof(valueinnumber));
+
+//"33"=>33;
+//true=>1,false=>0;
+//"33abc"=>NaN;
+
+let isloggedin=1;
+let con=Boolean(isloggedin);
+console.log(typeof con);
+
+let somenumber=33;
+let str=String(somenumber);
+console.log(typeof str);
