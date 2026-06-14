69d0c2030e49b2a12b8669cd2331704f875dad1a
Abhay979405 <abhaydwivedi5070@gmail.com>
2026-06-14 09:08:09 +0530

    number understanding

---
+++ 01_basic/number.js
@@ -0,0 +1,13 @@
+
+const num=100;
+const balance=new Number(400);
+console.log(balance);
+console.log(num)
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const othernumber=3456.345;
+console.log(othernumber.toPrecision(4));
+
+const value=1000000;
+console.log(value.toLocaleString('en-IN'));
