982764e51698e119169e2b0662caaf5b692b1b94
Abhay979405 <abhaydwivedi5070@gmail.com>
2026-06-14 08:14:15 +0530

    nonpremitive understanding

---
+++ 01_basic/datatype-summary.js
@@ -0,0 +1,10 @@
+//non-premitive datatypes
+const heros=["shaktiman","naagraj","doga"];
+let myobj={
+    name:"abhay",
+    age:20
+}
+
+const myfunction=function(){
+    console.log("hello world");
+}
