38d00cc83015eb52751345e12c0de6a732574b34
Abhay979405 <abhaydwivedi5070@gmail.com>
2026-06-14 10:09:45 +0530

    date understanding

---
+++ 01_basic/date.js
@@ -0,0 +1,11 @@
+// let mydate=new Date();
+// console.log(mydate.toString());
+// console.log(mydate.toISOString());
+// console.log(mydate.toUTCString());
+
+const mynewdate=new Date(2023,0,23);
+console.log(mynewdate.toString());
+
+let mytimestamp=Date.now();
+console.log(mytimestamp);
+console.log(Math.floor(Date.now()/1000));
