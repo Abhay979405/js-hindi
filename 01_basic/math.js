58d6aa6064c9457564c930223ff2efac8b3babdc
Abhay979405 <abhaydwivedi5070@gmail.com>
2026-06-14 09:29:44 +0530

    math understanding

---
+++ 01_basic/math.js
@@ -0,0 +1,11 @@
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.9));
+// let num=[4,6,98,23];
+// console.log(Math.min(...num))
+console.log(Math.min(4,6,3,2));
+console.log(Math.max(4,2,45,6));
+
+const min=10;
+const max=20;
+console.log(Math.floor(Math.random()*(max-min+1))+min);
