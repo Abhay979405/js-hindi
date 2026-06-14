7f8264ba4ec2e825cd8b9d2d136df2764f718d0b
Abhay979405 <abhaydwivedi5070@gmail.com>
2026-06-14 08:26:33 +0530

    memory understanding

--- 01_basic/datatype-summary.js
+++ 01_basic/datatype-summary.js
@@ -1,10 +1,27 @@
 //non-premitive datatypes
-const heros=["shaktiman","naagraj","doga"];
-let myobj={
+// const heros=["shaktiman","naagraj","doga"];
+// let myobj={
+//     name:"abhay",
+//     age:20
+// }
+
+// const myfunction=function(){
+//     console.log("hello world");
+// }
+
+//primitive(stack m ) non-primitive(heap) m
+let number=12344;
+let anothernumber=number;
+anothernumber=234;
+console.log(anothernumber);
+console.log(number);
+
+//non-primitive
+let obj={
     name:"abhay",
     age:20
 }
-
-const myfunction=function(){
-    console.log("hello world");
-}
+let obj2=obj
+obj2.age=25;
+console.log(obj.age);
+console.log(obj2.age);
